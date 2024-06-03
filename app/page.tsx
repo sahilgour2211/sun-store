a6eaa969e798859c68f4529f7d57d7fb12055b69
sahilmassive <sahil@gomassive.com>
2024-06-03 23:15:47 +0530

    initial setup

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import { Box } from "@mui/material";
+import React from "react";
+
+const page = () => {
+  return (
+    <Box sx={{ display: "flex", justifyContent: "center", marginTop: "32px" }}>
+      HOME
+    </Box>
+  );
+};
+
+export default page;
